052e7dccdcfdd583d02d06d8875fcf37d8f0c51f
DiptoDas8 <1205080.dd@ugrad.cse.buet.ac.bd>
2018-03-13 11:07:51 -0500

    v0.2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sarcasm
 
 Step 1: text to sarcasm score
+
+Step 2: image to text
+		install tesseract 3.02 from https://sourceforge.net/projects/tesseract-ocr-alt/files/
+		download language specific training data and add to tessdata folder
--- drive.py
+++ drive.py
@@ -1,4 +1,10 @@
-from text_sarcasm import text_sarcasm
+# from text_sarcasm import text_sarcasm
+from image_to_text import extract
+import os
 
+"""
 text_sarcasm_score = text_sarcasm('thank you!')
 print(text_sarcasm_score)
+"""
+
+extract(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.jpg'), lang="eng")
